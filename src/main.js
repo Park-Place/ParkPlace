b23c6765313c6b71fd84dbb022e3c462df5cac31
pereztjacob <pereztjacob@gmail.com>
2018-03-18 11:10:30 -0700

    fixing paths

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/reset.css';
 import './styles/main.css';
-import App from './components/App';
+import App from './components/app/App';
 import { Provider } from 'react-redux';
-import store from './store';
+import store from '../store/store';
 
 ReactDOM.render(
   <Provider store={store}>
