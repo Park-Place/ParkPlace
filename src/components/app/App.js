c7cf9bbf0279ad401358eb05667cd3c563555b07
G.Provost <grace.g.provost@gmail.com>
2018-03-19 10:24:34 -0700

    create presentation search component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './app.css';
+import Search from '../search/Search';
 
 export default class App extends Component {
 
@@ -15,29 +16,10 @@ export default class App extends Component {
           <h1></h1>
         </header>
         <main id="main" role="main">
+        <Search/>
         </main>
         <footer id="footer" role="contentinfo">
-          <ul>
-            <li>
-              <a href="https://github.com/Theartbug" target="_blank" rel="noopener noreferrer">
-                <span className="fa fa-github fa-3x"></span>
-                <span className="clip">Github</span>
-              </a>
-            </li>
-            <li>
-              <a href="https://www.linkedin.com/in/graceprovost/" target="_blank" rel="noopener noreferrer">
-                <span className="fa fa-linkedin fa-3x"></span>
-                <span className="clip">LinkedIn</span>
-              </a>
-            </li>
-            <li>
-              <a href="mailto:grace.g.provost@gmail.com">
-                <span className="fa fa-envelope-square fa-3x"></span>
-                <span className="clip">Email</span>
-              </a>
-            </li>
-          </ul>
-          <small>&copy; 2018 Grace Provost | Student Work</small>
+          <small>&copy; 2018 ParkPlace | Student Work</small>
         </footer>
       </div>
     );
