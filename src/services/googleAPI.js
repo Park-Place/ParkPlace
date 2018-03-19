7c1a14ed3a5c06f508d2465f2a7e76d6b22e4888
Christina Mills <chrimills@gmail.com>
2018-03-19 11:34:40 -0700

    hooking up google places/geocoding api

--- src/services/googleAPI.js
+++ src/services/googleAPI.js
@@ -1,4 +1,14 @@
-const BASE_URL = '';
+import { get } from './request';
+const BASE_MAPS_URL = 'https://maps.googleapis.com/maps/api/';
 const API_KEY = 'AIzaSyCcWQftr51E8GZR0nFG2N207HZm8cLIM2Y';
 
-export function
+
+export const getSearchByKeyword = (query) => get(`${BASE_MAPS_URL}place/textsearch/json?query=${query}&types=park&${API_KEY}`);
+
+export const getSearchByLocation = (query) => get(`${BASE_MAPS_URL}place/geocode/json?address=${encodeURI(query)}&${API_KEY}`);
+
+(lat, long) => get(`${BASE_MAPS_URL}place/nearbysearch/json?location=${lat},${long}&radius=500000&types=park&${API_KEY}`);
+
+
+
+
