140f666be52da74c8420184ec22d690feac9927e
Christina Mills <chrimills@gmail.com>
2018-03-20 11:13:33 -0700

    reducer search result test

--- src/components/search/actions.js
+++ src/components/search/actions.js
@@ -2,15 +2,10 @@ import { RESULTS_SET } from './reducers';
 import { getPlacesByTextSearch } from '../../services/googleAPI';
 
 export function searchByKeyword(query) {
-  console.log('AFTER function call!!!!');
 
   return dispatch => {
-    console.log('AFTER dispatch');
     getPlacesByTextSearch(encodeURI(query))
-
       .then(response => {
-        console.log('response', response);
-
         dispatch({
           type: RESULTS_SET,
           payload: response
@@ -19,15 +14,12 @@ export function searchByKeyword(query) {
   };
 }
 
+
 export function searchByLocation(query) {
-  console.log('AFTER function call!!!!');
 
   return dispatch => {
-    console.log('AFTER DISPATCH!!!!');
     getPlacesByTextSearch(encodeURI(query))
       .then(response => {
-        console.log('AFTER then!!!!');
-
         dispatch({
           type: RESULTS_SET,
           payload: response
