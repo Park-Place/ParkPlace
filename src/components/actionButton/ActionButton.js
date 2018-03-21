e84fb9784f917613ca020e3dd0bc2e93a38f4123
G.Provost <grace.g.provost@gmail.com>
2018-03-21 10:23:49 -0700

    review button deactivates after a review is added

--- src/components/actionButton/ActionButton.js
+++ src/components/actionButton/ActionButton.js
@@ -4,9 +4,9 @@ import './actionButton.css';
 export default class ActionButton extends Component {
 
   render() {
-    const { onClick, buttonText, type } = this.props;
+    const { onClick, buttonText, type, disabled } = this.props;
     return (
-      <button type={type} onClick={onClick}>{buttonText}</button>
+      <button type={type} onClick={onClick} disabled={disabled}>{buttonText}</button>
     );
   }
 }
