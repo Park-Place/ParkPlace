87be929f6b7cdfc61dc4fa4e30fa3a190c7b356a
pereztjacob <pereztjacob@gmail.com>
2018-03-22 11:01:21 -0700

    commiting again because of terminal error

--- src/components/user/Review.js
+++ src/components/user/Review.js
@@ -1,14 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { getParkImage } from '../../services/googleAPI';
-
+import ReviewForm from '../parkDetail/ReviewForm';
+import { auth } from '../../services/firebase';
 
 class Review extends Component {
 
+  state = {
+    editing: false
+  };
+
+  changeEditing = () => {
+    this.setState(prev => ({ editing: !prev.editing }));
+  };
+
   render() {
-    const { timeStamp, rating, review, parkObj } = this.props;
+    const { userObj, timeStamp, rating, review, parkObj, amenities, tags } = this.props;
     const { parkName, photoReference, parkId } = parkObj;
+    const { userId } = userObj;
+    const { editing } = this.state;
+
+    const uid = auth.currentUser ? auth.currentUser.uid : null;
+
+    const reviewObj = {
+      rating,
+      amenities: amenities.join(' '),
+      tags: tags.join(' '),
+      review
+    };
 
     return (
       <li className="user-review">
@@ -16,9 +36,17 @@ class Review extends Component {
           <h4>{parkName}</h4>
           <img src={getParkImage(photoReference, 500)} alt={parkName}/>
         </Link>
-        <p>{timeStamp}</p>
-        <p>{rating}</p>
-        <p>{review}</p>
+        {(uid === userId) && <button onClick={this.changeEditing}>{editing ? 'x' : <span className="fa fa-pencil"></span>}</button>}
+        {!editing && 
+        <Fragment>
+          <p>{timeStamp}</p>
+          <p>{rating}</p>
+          <p>{review}</p>
+        </Fragment>
+        }
+        {editing &&
+          <ReviewForm legendText={'Edit Your Review'} reviewObj={reviewObj} priorReview={true} handleClose={this.changeEditing}/>
+        }
       </li>
     );
   }
