524ee14238c4a037db798de2a5f983448416888d
Christina Mills <chrimills@gmail.com>
2018-03-22 12:03:40 -0700

    update form on user working

--- src/components/user/Review.js
+++ src/components/user/Review.js
@@ -20,6 +20,7 @@ class Review extends Component {
     const { parkName, photoReference, parkId } = parkObj;
     const { userId } = userObj;
     const { editing } = this.state;
+    console.log('photo', photoReference);
 
     const uid = auth.currentUser ? auth.currentUser.uid : null;
 
