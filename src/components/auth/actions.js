fbdbd5cdf32d017aac5f6905f5cd197e7f865563
G.Provost <grace.g.provost@gmail.com>
2018-03-23 10:25:16 -0700

    user image now updates after user logs in

--- src/components/auth/actions.js
+++ src/components/auth/actions.js
@@ -4,37 +4,49 @@ import { db, storage } from '../../services/firebase';
 
 const userAviStorage = storage.ref('users');
 const users = db.ref('users');
-const getUser = uid => users.child(uid).once('value').then(d => d.val());
+let userRef = null;
 
 export function listenForUser() {
   return dispatch => {
-    onUserStateChange(user =>
-      dispatch({
-        type: USER_SET,
-        payload: user ? getUser(user.uid) : null
-      })
-    );
+    onUserStateChange(user => {
+      if(!user && userRef) userRef.off();
+      if(user) {
+        userRef = users.child(user.uid);
+        userRef.on('value', d => {
+          dispatch(setUser(d.val()));
+        });
+      }
+      else dispatch(setUser(null));
+    });
+  };
+}
+
+function setUser(user) {
+  return {
+    type: USER_SET,
+    payload: user
   };
 }
 
 export function handleImageUpload(file, id) {
   const uploadTask = userAviStorage.child(id).put(file);
   const user = users.child(id);
+  return new Promise((resolve, reject) => {
 
-  uploadTask.on('state_changed', () => {
-    
-  }, err => {
-    this.setState({ error: err });
-  }, () => {
-    const downloadUrl = uploadTask.snapshot.downloadURL;
-    user.update({ 'image': downloadUrl });      
+    uploadTask.on('state_changed', () => {}, 
+      reject, 
+      () => {
+        const downloadUrl = uploadTask.snapshot.downloadURL;
+        user.update({ 'image': downloadUrl })
+          .then(resolve);      
+      });
   });
 }
 
 export function signup({ email, password, image, location, userName }) {
   return () => onSignUp(email, password) //create the user in auth
     .then(user => {
-      users.child(user.uid).set({ location, userName }) //create the user in firebase
+      return users.child(user.uid).set({ location, userName }) //create the user in firebase
         .then(() => handleImageUpload(image, user.uid)); //add the image to storage and then database
     });
 }
