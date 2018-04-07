3bbbfc9fab03e3f4fd88f26df39d336c8b66eb04
G.Provost <grace.g.provost@gmail.com>
2018-04-07 15:14:46 -0700

    clean up

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,6 @@
 const functions = require('firebase-functions');
 const BASE_MAPS_URL = 'https://maps.googleapis.com/maps/api/';
 const API_KEY = 'AIzaSyCcWQftr51E8GZR0nFG2N207HZm8cLIM2Y';
-const client = require('superagent');
 const cors = require('cors')();
 
 exports.getPlacesByTextSearch = functions.https.onRequest((req, res) => {
